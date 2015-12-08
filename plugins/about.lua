404bc3d0f2a83fd9fc0d901a3c2f843033b3e3eb
zhityer <barcelona20202@gmail.com>
2015-12-09 00:38:47 +0330

    Update and rename version.lua to about.lua

---
+++ plugins/about.lua
@@ -0,0 +1,18 @@
+do
+
+function run(msg, matches)
+  return 'PoliceTG '
+  برای سفارش گروه ضداسپم به ایدی زیر مراجعه کنید.
+  @policetg_admin
+end
+
+return {
+  description = "Shows bot version", 
+  usage = "!version: درباره ربات",
+  patterns = {
+    "^!version$"
+  }, 
+  run = run 
+}
+
+end
